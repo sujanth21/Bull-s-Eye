adc7222c1cfa14c61a471334732d54381c112f45
Sujanth <sujanth1987@gmail.com>
2017-07-22 12:55:08 +1000

    Added About Screen and close button

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Sujanth Sebamalaithasan on 22/7/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
